0212d7a52c4a5d82ed1365f95e0ed5b3bf3ae0ce
Esanki Thambawita <esankilakvindee2000@gmail.com>
2025-07-02 18:38:22 +0530

    Update TodoForm to handle task submission via props and form submission event

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const TodoForm = () => {
+export const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    // prevent default action
+    e.preventDefault();
+    if (value) {
+      // add todo
+      addTodo(value);
+      // clear form after submission
+      setValue("");
+    }
+  };
   return (
-    <form className="TodoForm">
+    <form onSubmit={handleSubmit} className="TodoForm">
       <input
         type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
         className="todo-input"
-        placeholder="What's the task today?"
+        placeholder="What is the task today?"
       />
       <button type="submit" className="todo-btn">
-        Add Todo
+        Add Task
       </button>
     </form>
   );
